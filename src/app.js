77dde3ad7e55665743a15b0928df575cfbf6160b
High23 <warenasb2@gmail.com>
2023-09-12 23:29:38 -0700

    Onload function now works

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-export { getForecast  };
+import {loadCurrentWeather, load3DayForecast, loadHourlyForecast, loadBackgroundImage} from './dom'
+
+export { getForecast, onLoad };
 // onLoad
 async function getForecast(location) {
     const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=668f1b7cfe154b44b6f30756230609&q=${location}&days=3`, {mode: 'cors'});
     const data = await response.json();
     return data;
 }
-/*
 
 function onLoad() {
     const weatherData = getForecast('london');
     weatherData.then(weather => {
-        console.log(weather.current);
-    });
-    weatherData.then(weather => {
-        console.log(weather.forecast);
+        loadCurrentWeather(weather);
+        loadBackgroundImage(weather.current);
+        load3DayForecast(weather.forecast);
+        loadHourlyForecast(weather);
     });
 }
 
-*/
